502fdfe92871ad23547b38f93e213b2b203ca742
BanhQuyDev <BanhQuyDev>
2022-01-18 12:49:01 +0700

    third

--- src/controllers/seats.controller.js
+++ src/controllers/seats.controller.js
@@ -1,4 +1,4 @@
-const {Seats,Showtime, sequelize,} = require("../models");
+const {Seat,Showtime, sequelize,} = require("../models");
 const { QueryTypes } = require("sequelize");
 const getListSeats = async (req, res) => {
     try {
@@ -18,8 +18,60 @@ const getListSeats = async (req, res) => {
         res.status(500).send(error);
     }
 }
-
+const getAllListSeats = async (req, res) => {
+    try {
+        const list = await Seat.findAll({});
+        res.status(200).send(list);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+const getDetailSeats = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const index = await Seat.findByPk(id);
+        res.status(200).send(index);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+const createSeats = async (req, res) => {
+    try {
+        const {id,name,status,price,type,showtimeId} = req.body;
+        const newSeats = await Seat.create({
+            id,name,status,price,type,showtimeId
+        });
+        res.status(201).send(newSeats);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+const updateSeats = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {name,status,price,type,showtimeId} = req.body;
+        await Seat.update({name,status,price,type,showtimeId},{where: { id }});
+        const index = await Seat.findByPk(id);
+        res.status(200).send(index);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+const deleteSeats = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const index = await Seat.findByPk(id);
+        await Seat.destroy({where: { id }});
+        res.status(200).send(index);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
 module.exports={
-    
-    getListSeats
+    getListSeats,
+    getAllListSeats,
+    getDetailSeats,
+    createSeats,
+    updateSeats,
+    deleteSeats
 }
--- src/routers/seats.router.js
+++ src/routers/seats.router.js
@@ -1,8 +1,20 @@
 const {Router}=require("express");
-const { getListSeats } = require("../controllers/seats.controller");
+const { getListSeats, getAllListSeats, getDetailSeats, createSeats, updateSeats, deleteSeats } = require("../controllers/seats.controller");
+const { checkExist } = require("../middlewares/validations/check-exist-middleware");
+const {Seat} = require("../models")
 const seatsRouter = Router();
 //http://localhost:7000/api/v1/seat
 seatsRouter.get("/",getListSeats);
+//http://localhost:7000/api/v1/seat/allList
+seatsRouter.get("/allList",getAllListSeats);
+//http://localhost:7000/api/v1/seat/:id
+seatsRouter.get("/:id",checkExist(Seat),getDetailSeats);
+//http://localhost:7000/api/v1/seat
+seatsRouter.post("/",createSeats);
+//http://localhost:7000/api/v1/seat/:id
+seatsRouter.put("/:id",checkExist(Seat),updateSeats);
+//http://localhost:7000/api/v1/seat/:id
+seatsRouter.delete("/:id",checkExist(Seat),deleteSeats);
 module.exports={
     seatsRouter,
 }
